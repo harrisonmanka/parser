6c14b80f1c4383cdfe3364b9c7fe04e1f46b3ad0
harrison <hmanka5@gmail.com>
2023-04-23 12:44:28 -0400

    more touches pt2

--- interpreter.c
+++ interpreter.c
@@ -33,32 +33,3 @@ int main(int argc, char* argv[]){
         final = bexpr(token);
     }
 }
-
-
-/**
-* Main function to loop through the text file and get each lexeme.
-*/
-int main(int argc, char* argv[]) {
-   //char  *token[TSIZE];      /* Spot to hold a token, fixed size */
-   char  input_line[LINE];     /* Line of input, fixed size        */
-   FILE  *in_file = NULL;      /* File pointer                     */
-   FILE  *out_file = NULL;
-   int   line_count;           /* Number of lines read             */
-
-  if (argc != 3) {
-    printf("Usage: tokenizer inputFile outputFile\n");
-    exit(1);
-  }
-
-  in_file = fopen(argv[1], "r");
-  if (in_file == NULL) {
-    fprintf(stderr, "ERROR: could not open %s for reading\n", argv[1]);
-    exit(1);
-  }
-
-  out_file = fopen(argv[2], "w");
-  if (out_file == NULL) {
-    fprintf(stderr, "ERROR: could not open %s for writing\n", argv[2]);
-    exit(1);
-  }
-}
